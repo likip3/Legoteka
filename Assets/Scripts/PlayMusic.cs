4e7d88cb48815795af4a16458fc22f0c85ba6387
Andreychegg <andrey.sukhov2014@yandex.ru>
2023-06-02 23:12:04 +0500

    Дизайн для выбора музыки

--- Assets/Scripts/PlayMusic.cs
+++ Assets/Scripts/PlayMusic.cs
@@ -3,18 +3,67 @@ using UnityEngine.UI;
 
 public class PlayMusic : MonoBehaviour
 {
-    public AudioClip audioClip;
-    public Button playButton;
+    public AudioClip[] audioClips;
+    public Button[] playButtons;
+    private Image[] buttonImages;
+    public Text[] buttonTexts;
+    public Sprite initialSprite;
+    public Sprite selectedSprite;
     public AudioSource audioSource;
 
     void Start()
     {
-        playButton.onClick.AddListener(PlayAudio);
+        buttonImages = new Image[playButtons.Length];
+
+        for (int i = 0; i < playButtons.Length; i++)
+        {
+            int index = i;
+            playButtons[i].onClick.AddListener(() => PlayAudio(index));
+            buttonImages[i] = playButtons[i].GetComponent<Image>();
+        }
+
+        CheckPlayingMusic();
     }
 
-    void PlayAudio()
+    void CheckPlayingMusic()
     {
-        audioSource.clip = audioClip;
-        audioSource.Play();
+        if (audioSource.isPlaying)
+        {
+            for (int i = 0; i < audioClips.Length; i++)
+            {
+                if (audioSource.clip == audioClips[i])
+                {
+                    buttonImages[i].sprite = selectedSprite;
+                    buttonTexts[i].color = Color.black;
+                    break;
+                }
+            }
+        }
+    }
+
+    void PlayAudio(int index)
+    {
+        if (audioSource.isPlaying && audioSource.clip == audioClips[index])
+        {
+            audioSource.Stop();
+            ResetButtonStates();
+        }
+        else if (index >= 0 && index < audioClips.Length)
+        {
+            audioSource.clip = audioClips[index];
+            audioSource.Play();
+            ResetButtonStates();
+            buttonImages[index].sprite = selectedSprite;
+            buttonTexts[index].color = Color.black;
+        }
+    }
+
+    void ResetButtonStates()
+    {
+        for (int i = 0; i < buttonImages.Length; i++)
+        {
+            buttonImages[i].sprite = initialSprite;
+            buttonTexts[i].color = Color.white;
+        }
     }
 }
